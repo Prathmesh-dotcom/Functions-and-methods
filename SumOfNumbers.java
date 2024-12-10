0d38ae46416b9bfbd1bbe8ff6a42a220c14414d0
Prathmesh dotcom <prathmeshasathe@gmail.com>
2024-12-10 11:31:32 +0530

    Sum of two no. by passing parameters

---
+++ SumOfNumbers.java
@@ -0,0 +1,9 @@
+public class SumOfNumbers {
+    public static int CalculateSum(int a, int b){
+        int sum= a+b;
+        return sum;
+    }
+    public static void main(String[] args) {
+        System.out.println(CalculateSum(25, 10));
+    }
+}

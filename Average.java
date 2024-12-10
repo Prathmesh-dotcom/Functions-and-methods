07c04f1e33fc9ffb5f149ba04524029a6d61f2e9
Prathmesh dotcom <prathmeshasathe@gmail.com>
2024-12-10 12:51:36 +0530

    Average of 3 numbers

---
+++ Average.java
@@ -0,0 +1,11 @@
+public class Average {
+    public static int Avg(int a, int b, int c){
+        return (a+b+c)/3;
+       
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Avg(5, 10, 15));
+
+    }
+}

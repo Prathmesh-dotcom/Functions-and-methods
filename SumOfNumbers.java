a225a19bb81ec2d82d0e68d8f70c1797f6c6cdf4
Prathmesh dotcom <prathmeshasathe@gmail.com>
2024-12-10 11:35:14 +0530

    sum and product of 2 no.

--- SumOfNumbers.java
+++ SumOfNumbers.java
@@ -3,7 +3,13 @@ public class SumOfNumbers {
         int sum= a+b;
         return sum;
     }
+
+    public static int Multiply(int a, int b){
+        int product=a*b;
+        return product;
+    }
     public static void main(String[] args) {
         System.out.println(CalculateSum(25, 10));
+        System.out.println(Multiply(25, 10));
     }
 }
